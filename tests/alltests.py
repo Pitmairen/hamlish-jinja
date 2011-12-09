9d26238229979f4224b5603c97c277002ef60d30
Richard Eames <reames@asymmetricventures.com>
2011-12-09 14:25:27 -0700

    - Add test for haml_tags

--- tests/alltests.py
+++ tests/alltests.py
@@ -8,7 +8,8 @@ if __name__ == '__main__':
 
     tests = [
         'test_debug_output', 'test_html_tags', 'test_jinja_tags',
-        'test_syntax', 'test_div_shortcut', 'test_compact_output'
+        'test_syntax', 'test_div_shortcut', 'test_compact_output',
+        'test_haml_tags'
     ]
 
     suite = unittest.TestLoader().loadTestsFromNames(tests)
