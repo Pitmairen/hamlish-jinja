de039fa0bceac4d80a8999d129f9bf63538060ba
Pitmairen <progrper@gmail.com>
2013-07-09 21:28:33 +0200

    Bump version

--- hamlish_jinja.py
+++ hamlish_jinja.py
@@ -10,7 +10,7 @@ import os.path
 from jinja2 import TemplateSyntaxError, nodes
 from jinja2.ext import Extension
 
-__version__ = '0.3.2'
+__version__ = '0.3.3-dev'
 
 
 begin_tag_rx = r'\{%\-?\s*haml.*?%\}'
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ from setuptools import setup
 
 setup(
     name='Hamlish-Jinja',
-    version='0.3.2',
+    version='0.3.3-dev',
     description='Haml-ish syntax for jinja2 templates',
     long_description=__doc__,
     author='Per Myren',
