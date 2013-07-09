b120d9b7b25aef783116907470f76dbbbda724f8
Pitmairen <progrper@gmail.com>
2013-07-09 21:17:09 +0200

    Add some tests

--- tests/test_jinja_tags.py
+++ tests/test_jinja_tags.py
@@ -2,7 +2,12 @@
 
 import unittest
 
+from jinja2 import Environment
+
 import testing_base
+
+from hamlish_jinja import Hamlish, Output, HamlishExtension
+
 
 
 class TestJinjaTags(testing_base.TestCase):
@@ -188,6 +193,87 @@ class TestJinjaTags(testing_base.TestCase):
         self.assertEqual(s, r)
 
 
+
+
+class TestJinjaTagsCustomPlaceholders(testing_base.TestCase):
+
+    def setUp(self):
+        self.hamlish = Hamlish(
+            Output(indent_string='  ', newline_string='\n',
+                  block_start_string='{$',
+                  block_end_string='$}',
+                  variable_start_string='${',
+                  variable_end_string='}'
+                  ))
+
+
+    def test_custom_block_string(self):
+
+        s = self._h('''-extends "test.haml"''')
+        r = '''{$ extends "test.haml" $}'''
+
+        self.assertEqual(s, r)
+
+        s = self._h('''-block title -> Test''')
+        r = '''{$ block title $}Test{$ endblock $}'''
+
+        self.assertEqual(s, r)
+
+
+    def test_custom_variable_string(self):
+
+        s = self._h('''=test''')
+        r = '''${ test }'''
+
+        self.assertEqual(s, r)
+
+
+    def _create_env(self):
+
+        env = Environment(extensions=[HamlishExtension],
+                         variable_start_string='${',
+                         variable_end_string='}',
+                         block_start_string='{$',
+                         block_end_string='$}')
+        env.hamlish_mode = 'indented'
+        env.hamlish_indent_string = '  '
+
+        return env
+
+    def test_custom_variable_string_with_environment(self):
+
+        env = self._create_env()
+
+
+        s = env.hamlish_from_string('''\
+%p
+  =test''').render(test='123')
+        r = '''\
+<p>
+  123
+</p>'''
+
+        self.assertEqual(s, r)
+
+
+
+    def test_custom_block_string_with_environment(self):
+
+        env = self._create_env()
+
+
+        s = env.hamlish_from_string('''\
+%ul
+  -for i in [1,2,3]:
+    %li -> =i
+''').render()
+
+        # Jinja adds some extra whitespace
+        r = '''\
+<ul>\n  \n    <li>1</li>\n  \n    <li>2</li>\n  \n    <li>3</li>\n  \n</ul>'''
+
+
+        self.assertEqual(s, r)
 
 
 if __name__ == '__main__':
